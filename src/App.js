8ec0b630103f86c9314d2c9c1953bb309696a3a7
Ivan HA <hangolam@gmail.com>
2018-06-03 23:44:20 +0800

    chore: remove unnecessary test and SW

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
