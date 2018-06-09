34070b3ad846ab97f636e9f401f1847b0fc67eba
Ivan HA <hangolam@gmail.com>
2018-06-09 16:44:47 +0800

    feat: prepare slot for suspense demo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,29 @@
 import React, { Component, Fragment } from 'react'
 
+import Tab from '../components/Tab'
+import Suspense from './Suspense'
 import TimeSlicing from './TimeSlicing'
 
 class App extends Component {
+  state = {
+    isTimeSlicing: true,
+  }
+
+  handleTabChange = isTimeSlicing => this.setState({ isTimeSlicing })
+
+  renderMainApp = () =>
+    this.state.isTimeSlicing ? <TimeSlicing /> : <Suspense />
+
   render() {
     return (
       <Fragment>
-        <TimeSlicing />
+        <Tab
+          isOn={this.state.isTimeSlicing}
+          onClick={this.handleTabChange}
+          leftValue={'Time Slicing Demo'}
+          rightValue={'Suspense Demo'}
+        />
+        {this.renderMainApp()}
       </Fragment>
     )
   }
---
+++ src/containers/Suspense/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Suspense extends Component {
+  render() {
+    return <div>Suspense demo WIP</div>
+  }
+}
+
+export default Suspense
