bfcf93da462463be6c725789c6704521620b8c4a
Ivan HA <hangolam@gmail.com>
2018-06-10 18:06:08 +0800

    feat: add delay in api request

--- src/helpers/githubApi.js
+++ src/helpers/githubApi.js
@@ -1,9 +1,15 @@
 import { USER_NAME } from '../constants'
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 export const fetchRepos = async () => {
   const response = await fetch(
     `https://api.github.com/users/${USER_NAME}/repos`
   )
+
+  // Simulate long delay
+  await sleep(3000)
+
   return response.json()
 }
 
@@ -11,5 +17,9 @@ export const fetchCommits = async repoName => {
   const response = await fetch(
     `https://api.github.com/repos/${USER_NAME}/${repoName}/commits`
   )
+
+  // Simulate long delay
+  await sleep(3000)
+
   return response.json()
 }
