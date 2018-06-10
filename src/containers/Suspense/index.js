212dc005cb7201201ca31878daccda2e25beeda6
Ivan HA <hangolam@gmail.com>
2018-06-10 15:58:47 +0800

    feat: add spinner for RepoListing

--- src/containers/Suspense/index.js
+++ src/containers/Suspense/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
+import Spinner from '../../components/Spinner'
 import { USER_NAME } from '../../constants'
+import { Placeholder } from '../../helpers/future'
 import RepoListing from './RepoListing'
 
 class Suspense extends Component {
   render() {
-    return <RepoListing userName={USER_NAME} />
+    return (
+      <Placeholder fallback={<Spinner size="large" />} delayMs={1000}>
+        <RepoListing userName={USER_NAME} />
+      </Placeholder>
+    )
   }
 }
 
